5f15d3081649cb6b0ba965e8969b13dc6e7e766e
Monisha <79446150+Monisha36@users.noreply.github.com>
2023-07-28 13:31:33 +0530

    Style added for sign in page

--- src/component/sign-in-form/sign-in-form.component.js
+++ src/component/sign-in-form/sign-in-form.component.js
@@ -48,7 +48,7 @@ const SignInForm = () => {
         setFormFields({ ...formFields, [name]: value });
     };
     return (
-        <div className="sign-up-container">
+        <div className="sign-in-container">
             <h2>Already have an account?</h2>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
